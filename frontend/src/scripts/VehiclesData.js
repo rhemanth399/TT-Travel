41237b55a8393c7108c9d2c3361ff5d2cec12d41
rhemanth <rhemanth399@gmail.com>
2024-10-02 19:10:39 +0530

    "vehicle data added"

--- frontend/src/scripts/VehiclesData.js
+++ frontend/src/scripts/VehiclesData.js
@@ -9,62 +9,111 @@ export const vehicles = [
   {
     name: 'Swift Dzire Vehicle',
     image: SwiftDzire,
-    ratePerKilometer: 14.00,
-    maxPeople: 4,
+    ratePerKilometer: 17.00,
+    maxPeople: 4+1,
     transmission: 'Manual',
     airConditioned: true,
     driverAllowance: 300,
-    phoneNumber: '+919391711883'
+    phoneNumber: '+918885544518'
   },
   {
     name: 'Innova Vehicle',
     image: Innova,
-    ratePerKilometer: 18.00,
-    maxPeople: 6,
+    ratePerKilometer: 20.00,
+    maxPeople: 6+1,
     transmission: 'Automatic',
     airConditioned: true,
     driverAllowance: 500,
-    phoneNumber: '+919876543210'
+    phoneNumber: '+918885544518'
   },
   {
     name: 'Etios Vehicle',
     image: Etios,
-    ratePerKilometer: 18.00,
-    maxPeople: 6,
+    ratePerKilometer: 17.00,
+    maxPeople: 4+1,
     transmission: 'Automatic',
     airConditioned: true,
     driverAllowance: 500,
-    phoneNumber: '+919876543210'
+    phoneNumber: '+918885544518'
   },
   {
     name: 'HyundaiCreta Vehicle',
     image: HyundaiCreta,
     ratePerKilometer: 18.00,
-    maxPeople: 6,
+    maxPeople: 4+1,
     transmission: 'Automatic',
     airConditioned: true,
     driverAllowance: 500,
-    phoneNumber: '+919876543210'
+    phoneNumber: '+918885544518'
   },
   {
     name: 'Tempo Vehicle',
     image: Tempo,
-    ratePerKilometer: 18.00,
-    maxPeople: 6,
+    ratePerKilometer: 25.00,
+    maxPeople: 12+1,
     transmission: 'Automatic',
     airConditioned: true,
     driverAllowance: 500,
-    phoneNumber: '+919876543210'
+    phoneNumber: '+918885544518'
   },
-
+  {
+    name: 'Tempo Vehicle',
+    image: Tempo,
+    ratePerKilometer: 30.00,
+    maxPeople: 17+1,
+    transmission: 'Automatic',
+    airConditioned: true,
+    driverAllowance: 500,
+    phoneNumber: '+918885544518'
+  },
+  {
+    name: 'Tempo Vehicle',
+    image: Tempo,
+    ratePerKilometer: 40.00,
+    maxPeople: 20+1,
+    transmission: 'Automatic',
+    airConditioned: true,
+    driverAllowance: 500,
+    phoneNumber: '+916300330155'
+  },
+  {
+    name: 'Tempo Vehicle',
+    image: Tempo,
+    ratePerKilometer: 42.00,
+    maxPeople: 26+1,
+    transmission: 'Automatic',
+    airConditioned: true,
+    driverAllowance: 500,
+    phoneNumber: '+916300330155'
+  },
   {
     name: 'Bus Vehicle',
     image: Bus,
-    ratePerKilometer: 18.00,
-    maxPeople: 6,
+    ratePerKilometer: 50.00,
+    maxPeople: 32+1,
     transmission: 'Automatic',
     airConditioned: true,
     driverAllowance: 500,
-    phoneNumber: '+919876543210'
+    phoneNumber: '+916300330155'
+  },
+  {
+    name: 'Bus Vehicle',
+    image: Bus,
+    ratePerKilometer: 60.00,
+    maxPeople: 40+1,
+    transmission: 'Automatic',
+    airConditioned: true,
+    driverAllowance: 500,
+    phoneNumber: '+916300330155'
+  },
+  {
+    name: 'Bus Vehicle',
+    image: Bus,
+    ratePerKilometer: 65.00,
+    maxPeople: 55+1,
+    transmission: 'Automatic',
+    airConditioned: true,
+    driverAllowance: 500,
+    phoneNumber: '+916300330155'
   },
 ];
