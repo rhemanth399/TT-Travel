da9c8a0fd1f56e5d20190e0eefd3efc4c2d0a82b
rhemanth <rhemanth399@gmail.com>
2024-10-02 18:50:37 +0530

    "third commit"

--- frontend/src/scripts/VehiclesData.js
+++ frontend/src/scripts/VehiclesData.js
@@ -2,6 +2,8 @@ import SwiftDzire from '../assets/SwiftDzire.jpg';
 import Innova from '../assets/Innova.jpg';
 import Tempo from '../assets/Tempo.jpg';
 import Bus from '../assets/EicherBus.jpg'
+import Etios from '../assets/etioscar.jpg';
+import HyundaiCreta from '../assets/HyundaiCreta.jpg'
 
 export const vehicles = [
   {
@@ -17,6 +19,26 @@ export const vehicles = [
   {
     name: 'Innova Vehicle',
     image: Innova,
+    ratePerKilometer: 18.00,
+    maxPeople: 6,
+    transmission: 'Automatic',
+    airConditioned: true,
+    driverAllowance: 500,
+    phoneNumber: '+919876543210'
+  },
+  {
+    name: 'Etios Vehicle',
+    image: Etios,
+    ratePerKilometer: 18.00,
+    maxPeople: 6,
+    transmission: 'Automatic',
+    airConditioned: true,
+    driverAllowance: 500,
+    phoneNumber: '+919876543210'
+  },
+  {
+    name: 'HyundaiCreta Vehicle',
+    image: HyundaiCreta,
     ratePerKilometer: 18.00,
     maxPeople: 6,
     transmission: 'Automatic',
